fc481a3bf78569e210eb659d427cbec7a6824ea9
nearly <nearly@trinity.edu>
2024-06-02 13:59:48 -0500

    basic setup

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('FLASK server running!')
+
+
+if __name__ == '__main__':
+    app.run()
